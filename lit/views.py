c573b4223ee120a9885bb3db99c61a1bcc11c06d
valouugit <valentingoulier@gmail.com>
2022-07-06 22:31:17 +0200

    Ajout de l'interface de login & middleware login

--- lit/views.py
+++ lit/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'lit/index.html')
--- litreview/urls.py
+++ litreview/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('lit/', include('lit.urls')),
+    path('', include('lit.urls')),
     path('admin/', admin.site.urls),
+    path("", include("django.contrib.auth.urls")),
 ]
